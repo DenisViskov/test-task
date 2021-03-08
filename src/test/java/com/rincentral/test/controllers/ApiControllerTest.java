e96cfaecbf5be077a155b2ca1e0a74d650368e9b
denisviskov <denisviskov.developer@gmail.com>
2021-03-08 15:45:46 +0300

    added getCarsTest

--- src/test/java/com/rincentral/test/controllers/ApiControllerTest.java
+++ src/test/java/com/rincentral/test/controllers/ApiControllerTest.java
@@ -2,14 +2,22 @@ package com.rincentral.test.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rincentral.test.TestApplication;
+import com.rincentral.test.models.BodyCharacteristics;
+import com.rincentral.test.models.CarFullInfo;
+import com.rincentral.test.models.EngineCharacteristics;
+import com.rincentral.test.models.external.enums.EngineType;
+import com.rincentral.test.models.external.enums.FuelType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Collections;
+
 import static com.rincentral.test.controllers.params.QueryParams.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(classes = TestApplication.class)
@@ -23,6 +31,32 @@ class ApiControllerTest {
 
     @Test
     void getCars() throws Exception {
+        CarFullInfo carFullExpected = CarFullInfo.builder()
+                                                 .id(15)
+                                                 .segment("B-segment")
+                                                 .brand("Skoda")
+                                                 .model("Rapid")
+                                                 .country("Czech Republic")
+                                                 .generation("I generation")
+                                                 .modification("1.4 TSI")
+                                                 .bodyCharacteristics(
+                                                         BodyCharacteristics.builder()
+                                                                            .bodyHeight(1474)
+                                                                            .bodyLength(4483)
+                                                                            .bodyStyle("Hatchback")
+                                                                            .bodyWidth(1706)
+                                                                            .build()
+                                                 )
+                                                 .engineCharacteristics(
+                                                         EngineCharacteristics.builder()
+                                                                              .engineDisplacement(1395)
+                                                                              .engineType(EngineType.L4)
+                                                                              .fuelType(FuelType.GASOLINE)
+                                                                              .hp(125)
+                                                                              .build()
+                                                 )
+                                                 .build();
+
         mockMvc.perform(get("/api/cars")
                 .queryParam(COUNTRY.getParam(), "Czech Republic")
                 .queryParam(SEGMENT.getParam(), "B-segment")
@@ -32,12 +66,16 @@ class ApiControllerTest {
                 .queryParam(MIN_MAX_SPEED.getParam(), "200")
                 .queryParam(SEARCH.getParam(), "1.4 TSI")
                 .queryParam(IS_FULL.getParam(), "true")
-                .queryParam(BODY_STYLE.getParam(),"Hatchback"))
-               .andExpect(status().isOk());
+                .queryParam(BODY_STYLE.getParam(), "Hatchback"))
+               .andExpect(status().isOk())
+               .andExpect(content().json(objectMapper.writeValueAsString(Collections.singletonList(carFullExpected))));
 
+        // Expected error message
         mockMvc.perform(get("/api/cars")
-                .queryParam(SEGMENT.getParam(), "B-segment"))
-               .andExpect(status().isOk());
+                .queryParam(SEGMENT.getParam(), "B-segment")
+                .queryParam(YEAR.getParam(), "someWord"))
+               .andExpect(status().is4xxClientError())
+               .andExpect(content().string("Wrong given parameters"));
     }
 
     @Test
